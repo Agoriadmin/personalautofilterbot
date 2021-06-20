6228514b1be8107c08b14d446ea70c27f6d910e9
Agoriadmin <86073822+Agoriadmin@users.noreply.github.com>
2021-06-20 13:17:29 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = Thankyou For Using Our Service Please Support Us By Sharing Our Group Link To Your Friends  👇 \n @agorimovies \n @mv_mania \n @agorihome",
+                caption = f"{file_name} \n @agorimovies \n @mv_mania \n @agorihome",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
