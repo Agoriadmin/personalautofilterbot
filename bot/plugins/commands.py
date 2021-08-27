b668180b24a5595ea4ff1ca933fdd78228b1df66
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2021-08-27 20:19:36 +0530

    Update and rename commands.py to commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -2,35 +2,17 @@
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG & NO-ONE-KN0WS-ME & MRRONS
 
-from pyrogram import filters, Client
+from pyrogram import filters
+from pyrogram import Client as Mai_bOTs
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram.errors import UserNotParticipant
 from bot import Translation # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
+#from bot import ADMINS
 
 db = Database()
 
-@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+@Mai_bOTs.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-    update_channel = "@agorihome"
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked out":
-               await update.reply_text("🤭 Sorry Dude, You are B A N N E D 🤣🤣🤣")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="<b>🤭 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭\n\nനിങ്ങൾക് സിനിമകൾ വേണോ? അതിനായി ആദ്യം നിങ്ങൾ ചെയ്യേണ്ടത് ഞങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യുക എന്നതാണ്🤭... 😁\n\nJoin ചെയ്തതിന് ശേഷം വീണ്ടും ബോട്ട് /start ആക്കൂ AND FEEL THE MAGIC.😁</b>",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text=" 🤭JOIN OUR CHANNEL🤭 ", url=f"https://t.me/agorihome")]
-              ])
-            )
-            return
-        except Exception:
-            await update.reply_text("Something Wrong. Contact my Support Group")
-            return
     
     try:
         file_uid = update.command[1]
@@ -146,7 +128,7 @@ async def start(bot, update):
     )
 
 
-@Client.on_message(filters.command(["help"]) & filters.private, group=1)
+@Mai_bOTs.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
     buttons = [[
         InlineKeyboardButton('Close 🔐', callback_data='close')
@@ -163,7 +145,7 @@ async def help(bot, update):
     )
 
 
-@Client.on_message(filters.command(["about"]) & filters.private, group=1)
+@Mai_bOTs.on_message(filters.command(["about"]) & filters.private, group=1)
 async def about(bot, update):
     
     buttons = [[
@@ -180,7 +162,7 @@ async def about(bot, update):
         reply_to_message_id=update.message_id
     )
 
-@Client.on_message(filters.text & ~ filters.command(["start", "help"]) & filters.private & ~ filters.me)
+@Mai_bOTs.on_message(filters.text & ~ filters.command(["start", "help"]) & filters.private & ~ filters.me)
 async def note(bot, update):
     buttons = [[
         InlinekeyboardButton('MOVIE REQUEST 💣', url='https://t.me/agorimovies')
